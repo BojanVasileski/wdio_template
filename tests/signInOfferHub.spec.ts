85a582f7dfce0ee83b5bea65cefa8c545a5efe53
BojanVasileski <50120823+BojanVasileski@users.noreply.github.com>
2022-06-27 19:36:31 -0400

    breaking-the-code
    woops

--- tests/signInOfferHub.spec.ts
+++ tests/signInOfferHub.spec.ts
@@ -3,16 +3,17 @@ import { arrangeTestSession, waitForUrlToInclude } from '../utility/helpers'
 import signInPage from '../pages/sign-in'
 
 describe('Sign In', () => {
- 
+
     // ******** We shouldn't test the Ping sign on. This is for example purposes only
     before(() => {
         arrangeTestSession(signInPage.url);
     });
 
-    it('A user can sign in with valid credentials', () => {     
+    it('A user can sign in with valid credentials', () => {
         signInPage.usernameInput.addValue(settings.username);
         signInPage.passwordInput.addValue(settings.password);
         signInPage.submitButton.click();
+        break.theCode();
 
         //Wait for URL to include /queue
         waitForUrlToInclude('/queue');
